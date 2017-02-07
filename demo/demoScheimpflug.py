1b52e86e7985613928ef6e1114d1c760ab2fd202
LukasHoghoj <lukashoghoj@hotmail.com>
2017-02-07 15:30:26 +0100

    demoScheimpflug created

---
+++ demo/demoScheimpflug.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb  7 15:25:59 2017
+
+@author: Lukas Høghøj
+
+Demonstration on how to use the Scheimpflug with the par2vel toolbox
+"""
+
--- run.py
+++ run.py
@@ -2,7 +2,7 @@
 """
 Created on Tue Feb  7 15:02:07 2017
 
-@author: Lukas
+@author: Lukas Høghøj
 
 This file calls the demo script, that is located in a folder that is sibling 
 with the ones, the objects it calls are contained in.
