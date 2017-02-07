ce1490e0687ac5b3e46591edc0395d01fb96db99
LukasHoghoj <lukashoghoj@hotmail.com>
2017-02-07 15:05:41 +0100

    Created run file

---
+++ run.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb  7 15:02:07 2017
+
+@author: Lukas
+
+This file calls the demo script, that is located in a folder that is sibling 
+with the ones, the objects it calls are contained in.
+"""
+
+
+from demo import demo
