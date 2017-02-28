ffc32c9e444ef12f80c124ebfef505580fd147fc
LukasHoghoj <lukashoghoj@hotmail.com>
2017-02-28 14:14:26 +0100

    Small bugfixes

--- par2vel/piv3d.py
+++ par2vel/piv3d.py
@@ -6,13 +6,13 @@ import numpy as np
 import scipy
 
 from par2vel.field import Field3D, Field2D
-from par2vel.Camera import Camera
+from par2vel.camera import Camera
 from par2vel.piv2d import fftdx
 
-def piv_camplane(Im_11,Im_12,Im_21,Im_22,field3d):
+def piv_camplane(Im_cam1,Im_cam2,field3d):
     """piv_camplane(Im_11,Im_12,Im_21,Im_22,field3d)
     Function that finds the displacements in both image planes and appends them
     to their field2d objects, that are contained in the global field3d"""
-    fftdx(Im_11,Im_12,field3d.field2d[0])
-    fftdx(Im_21,Im_22,field3d.field2d[1])
+    fftdx(Im_cam1[0],Im_cam1[1],field3d.field2d[0])
+    fftdx(Im_cam2[0],Im_cam2[1],field3d.field2d[1])
 
