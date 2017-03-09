ac18edcd76d30d341d48067292f3fa2382646acb
LukasHoghoj <lukashoghoj@hotmail.com>
2017-03-09 13:19:22 +0100

    Expanded unit test (further tests to be added)

--- tests/test_field3D.py
+++ tests/test_field3D.py
@@ -19,6 +19,21 @@ class testField3D(unittest.TestCase):
         cam2.set_calibration(-numpy.pi/4,1/500)
         field = Field3D([cam1,cam2])
         field.grid([25,20])
+        X_1 = field.field2d[0].camera.x2X(field.field2d[0].xflat())
+        X_2 = field.field2d[1].camera.x2X(field.field2d[1].xflat())
+        for i in range(500):
+            for k in range(3):
+                self.assertAlmostEqual(X_1[k,i],X_2[k,i])
+                
+    def testpartial(self):
+        cam1 = Scheimpflug((32,32))
+        cam2 = Scheimpflug((32,32))
+        cam1.set_calibration(numpy.pi/3,1/100)
+        cam2.set_calibration(-numpy.pi/3,1/100)
+        field = Field3D([cam1,cam2])
+        field.grid([2,2])
+        field.dxdX()
+        
 
 if __name__ == '__main__':
     unittest.main()
