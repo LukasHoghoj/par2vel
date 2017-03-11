c6e607bd9ef26068bee4c470b95223bde80b2a1c
LukasHoghoj <lukashoghoj@hotmail.com>
2017-03-11 18:42:17 +0100

    Finished unittest for the stereo function in piv3d.py

--- tests/test_piv3d.py
+++ tests/test_piv3d.py
@@ -5,30 +5,24 @@ from par2vel.field import Field3D
 from par2vel.piv3d import stereo
 from par2vel.artimage import X_U
 
-def dis_func(X)
-	s = X.shape
-	X.reshape((3,-1))
-	dX = zeros(s)
-	dX[0] = X[0]/100
-	dX[1] = X[1]/100
-	dX[2] = X[0]/100
-	dX.reshape((3,s[2],s[2]))
-	return dX
+
 
 
 class StereoTest(unittest.TestCase):
-	
-	def TestStereo(self):
+	def testStereo(self):
 		cam1 = Scheimpflug((512,512))
 		cam2 = Scheimpflug((512,512))
 		cam1.set_calibration(numpy.pi/3,1/200)
 		cam2.set_calibration(-numpy.pi/3.1,1/250)
 		field = Field3D([cam1,cam2])
 		field.grid([3,4])
-		dX = dis_func(field.X)
-		dXflat = dX.reshape((3,-1))
+		dX_flat = field.getX_flat()/100
+		dX = dX_flat.reshape(field.X.shape)
 		X_flat = field.X.reshape((3,-1))
 		field.field2d[0].dx = field.field2d[0].camera.dX2dx(X_flat,dX_flat).reshape((2,4,3))
 		field.field2d[1].dx = field.field2d[1].camera.dX2dx(X_flat,dX_flat).reshape((2,4,3))
 		stereo(field)
-		self.AssertAlmostEqual(dX,field.dX)
+		numpy.testing.assert_array_almost_equal(dX,field.dX)
+
+if __name__ == '__main__':
+    unittest.main(exit = False)
