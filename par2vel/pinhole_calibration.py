65d853440af4792782c8e877e355afd9b971c31a
LukasHoghoj <lukashoghoj@hotmail.com>
2017-03-16 10:11:37 +0100

    Created function that finds the rotation and translation matrix, that fits the best

--- par2vel/pinhole_calibration.py
+++ par2vel/pinhole_calibration.py
@@ -19,7 +19,7 @@ def Distortion(x_d, x_n):
     i = np.indices(len * 2)
     lhs[i % 2 == 0] = x_d[0 , :] - x_n[0 , :]
     lhs[i % 2 == 1] = x_d[1 , :] - x_n[1 , :]
-    rhs = np.zeros((len * 2,5))
+    rhs = np.zeros((len * 2 , 5))
     r = (x_n[0 , :] ** 2 + x_n[1 , :] ** 2)
     rhs[: , 0][i % 2 == 0] = x_n[0 , :] * r
     rhs[: , 0][i % 2 == 1] = x_n[1 , :] * r
@@ -35,4 +35,32 @@ def Distortion(x_d, x_n):
     dis = lstsq(rhs,lhs)[0]
     return dis
 
-def Rotation_T()
+def Rotation_T(x_n, X_p):
+    """Function that finds the rotation and translation parameters """
+    assert x_n.shape[1] == X_p.shape[1]
+    len = x_n.shape[1]
+    lhs = np.zeros(len * 2)
+    rhs = np.zeros((len * 2 , 12))
+    i = np.indices(len * 2)
+    rhs[: , 0 : 3][i % 2 == 0] = X_p
+    rhs[: , 3][i % 2 == 0] = 1
+    rhs[: , 0 : 4][i % 2 == 1] = 0
+    rhs[: , 4 : 8][i % 2 == 0] = 0
+    rhs[: , 4 : 7][i % 2 == 1] = X_p
+    rhs[: , 7][i % 2 == 1] = 1
+    rhs[: , 8][i % 2 == 0] = - x_n[0 , :] * X_p[0 , :]
+    rhs[: , 8][i % 2 == 1] = - x_n[1 , :] * X_p[0 , :]
+    rhs[: , 9][i % 2 == 0] = - x_n[0 , :] * X_p[1 , :]
+    rhs[: , 9][i % 2 == 1] = - x_n[1 , :] * X_p[1 , :]
+    rhs[: , 10][i % 2 == 0] = - x_n[0 , :] * X_p[2 , :]
+    rhs[: , 10][i % 2 == 1] = - x_n[1 , :] * X_p[2 , :]
+    rhs[: , 11][i % 2 == 0] = - x_n[0 , :]
+    rhs[: , 11][i % 2 == 1] = - x_n[1 , :]
+
+    RT = lstsq(rhs,lhs)[0]
+    R = np.zeros((3,4))
+    R[0] = RT[0 : 4]
+    R[1] = RT[4 : 8]
+    R[2] = RT[8 : 12]
+
+    return R
