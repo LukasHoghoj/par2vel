41caecb6de8a12e767528d1b780c841b25c713b7
LukasHoghoj <lukashoghoj@hotmail.com>
2017-03-23 13:06:19 +0100

    Transforeddebugging script to unittest (fails at the moment, probably because of bad calibratiob

--- tests/test_pincali.py
+++ tests/test_pincali.py
@@ -4,12 +4,24 @@ from par2vel.pinhole_calibration import Calibrate_Pinhole
 import numpy as np
 import unittest
 
-cam = Scheimpflug((512,512))
-cam.set_calibration(np.pi/3,1/100) 
-X = np.random.rand(3,20) - 0.5
-X[0] = X[0] * 2
-X[1] = X[1] * 2
-X[2] = X[1] * 0.005
-x = cam.X2x(X)
-C = np.array([[0.06, 0 , 256] , [0 , 0.06 , 256]])
-a , b , c = Calibrate_Pinhole(X , x , C)
+
+class Calibrationtest(unittest.TestCase):
+    def testCalibration(self):
+        cam = Scheimpflug((512,512))
+        cam.set_calibration(np.pi/3,1/100) 
+        X = np.random.rand(3,20) - 0.5
+        X[0] = X[0] * 2
+        X[1] = X[1] * 2
+        X[2] = X[1] * 0.005
+        x = cam.X2x(X)
+        C = np.array([[0.06, 0 , 256] , [0 , 0.06 , 256]])
+        R, dis ,C = Calibrate_Pinhole(X , x , C)
+        pincam = Pinhole((512,512))
+        pincam.manual_calibration(R, dis, C)
+        X_test = np.array([[0.2, 1, -2, 0],[2, -1, 0, 0.1],[0.01, -0.04, -0.001, 0.02]])
+        x_sch = cam.X2x(X_test)
+        x_pin = pincam.X2x(X_test)
+        numpy.testing.assert_array_almost_equal(x_pin,x_sch)
+
+if __name__ == '__main__':
+    unittest.main(exit = False)
