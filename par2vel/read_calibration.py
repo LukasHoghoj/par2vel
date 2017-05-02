6d59132267fe3806169f00f3e3140d7309fe75aa
LukasHoghoj <lukashoghoj@hotmail.com>
2017-05-02 08:56:32 +0200

    Broke lines such that the code fits into the typical python formatting

--- par2vel/read_calibration.py
+++ par2vel/read_calibration.py
@@ -1,12 +1,12 @@
 class Calibration_image(object):
     def __init__(self, img_name):
-        """The Calibration_image is called as Calibration_image(img_name), where
-        img_name is the path to the calibration image. 
+        """The Calibration_image is called as Calibration_image(img_name), 
+        where img_name is the path to the calibration image. 
         The purpose of this object is to read an image of a carthesian grid, 
-        where each point is represented by a dot. Call the object in order to open
-        an image.
-        It may be necessary to do some preleminary picture processing in order to
-        eliminate shadows.
+        where each point is represented by a dot. Call the object in order to 
+        open an image.
+        It may be necessary to do some preleminary picture processing in order
+        to eliminate shadows.
         
         >>> self = Calibration_image(filename)
         """
@@ -18,16 +18,18 @@ class Calibration_image(object):
         self.img_name = img_name
     
     def find_ellipses(self, edge_dist = [15, 15, 15, 15]):
-        """The find_ellipses function detects elipse shapes, that correspond to dots,
-        in the image. The center coordinates of all elipses, which lie in the 
-        acceptebale windows are returned in the array self.x. 
-        The optional argument edge_dist can be used to determine how close to the edge
-        the centerpoints may lie. The distances are defined in pixels on the image plane
+        """The find_ellipses function detects elipse shapes, that correspond to
+        dots, in the image. The center coordinates of all elipses, which lie in
+        the acceptebale windows are returned in the array self.x. 
+        The optional argument edge_dist can be used to determine how close to 
+        the edge the centerpoints may lie. The distances are defined in pixels
+        on the image plane
         
         >>> self.find_ellipses(edge_dist = [d_top, d_bottom, d_left, d_right])
         """
 
-        from cv2 import Canny, findContours, fitEllipse, RETR_EXTERNAL, CHAIN_APPROX_NONE
+        from cv2 import Canny, findContours, fitEllipse, RETR_EXTERNAL, \
+                        CHAIN_APPROX_NONE
         from numpy import zeros
 
         # If edge list is only a scalar, use the distance on all sides
@@ -45,8 +47,10 @@ class Calibration_image(object):
         for ind, cont in enumerate(contours):
             elpsa.append(fitEllipse(cont))
             # If the centerpoint is not to close to the edge
-            if elpsa[ind][0][0] > edge_dist[2] and elpsa[ind][0][0] < self.shape[1] - edge_dist[3] and\
-               elpsa[ind][0][1] > edge_dist[0] and elpsa[ind][0][1] < self.shape[0] - edge_dist[1]:
+            if elpsa[ind][0][0] > edge_dist[2] and\
+               elpsa[ind][0][0] < self.shape[1] - edge_dist[3] and\
+               elpsa[ind][0][1] > edge_dist[0] and\
+               elpsa[ind][0][1] < self.shape[0] - edge_dist[1]:
                 # Add centerpoint to previoulsy created list
                 x.append(elpsa[ind][0][0])
                 y.append(elpsa[ind][0][1])
@@ -65,7 +69,8 @@ class Calibration_image(object):
         >>> self.coordinate_system()
         """
 
-        from matplotlib.pyplot import figure, imshow, title, plot, show, pause, subplot2grid, text, axis
+        from matplotlib.pyplot import figure, imshow, title, plot, show, \
+                                      pause, subplot2grid, text, axis
         from numpy import array
 
         self.selected_x = []
@@ -74,26 +79,32 @@ class Calibration_image(object):
         self.fig = figure(1)
         self.ax_im = subplot2grid((10, 3), (0,0), colspan = 3, rowspan = 9)
         imshow(self.img,'gray')
-        title(("Please click on the following points in the same order as mentioned: (0,0), (1,0), (0,1)\n " +\
-               "Then click on points that have to be exclude from the axes, such that all defined points\n" +\
+        title(("Please click on the following points in the same order as"+\
+               "mentioned: (0,0), (1,0), (0,1)\n " +\
+               "Then click on points that have to be exclude from the"+\
+               " axes, such that all defined points\n" +\
                "on the axes belong to complete lines of dots"), fontsize = 8)
         plot(self.x[0, :],self.x[1, :],'rx')
         axis('off')
         self.ax_done = subplot2grid((10, 3), (9,0))
-        text(0.5, 0.5,'Done',horizontalalignment='center',verticalalignment='center')
+        text(0.5, 0.5,'Done',horizontalalignment='center',\
+                             verticalalignment='center')
         axis([0, 1, 0, 1])
         axis('off')
         self.ax_close = subplot2grid((10, 3), (9, 1))
-        text(0.5, 0.5,'Done and close figure',horizontalalignment='center',verticalalignment='center')
+        text(0.5, 0.5, 'Done and close figure', horizontalalignment = 'center'\
+                        , verticalalignment='center')
         axis([0, 1, 0, 1])
         axis('off')
         self.ax_del = subplot2grid((10, 3), (9, 2))
-        text(0.5, 0.5,'Cancel last selected point',horizontalalignment='center',verticalalignment='center')
+        text(0.5, 0.5, 'Cancel last selected point',\
+                 horizontalalignment = 'center', verticalalignment = 'center')
         axis([0, 1, 0, 1])
         axis('off')
         show()
         pause(0.01)
-        self.cid = self.fig.canvas.mpl_connect('button_press_event', self.onclick)
+        self.cid = self.fig.canvas.mpl_connect('button_press_event',\
+                                               self.onclick)
         # Wait for the user to select 3 points:
         self.waitcanvas()
         # Delete ellipses the user does not wants to keep:
@@ -122,14 +133,16 @@ class Calibration_image(object):
         """The main purpose of this function is to make the program wait 
         before executing the rest of the scripts, such that the user has time
         enough to make the 3 clicks.
-        Note that this function is automatically called in self.coordinate_system()
+        Note that this function is automatically called in 
+        self.coordinate_system()
         """
 
         from matplotlib.pyplot import pause, close
 
         while True:
             pause(0.01)
-            self.cid = self.fig.canvas.mpl_connect('button_press_event', self.onclick)
+            self.cid = self.fig.canvas.mpl_connect('button_press_event',\
+                                                   self.onclick)
             if self.quit_figure == 'done' and len(self.selected_x) >= 3:
                 break
             if self.quit_figure == 'close' and len(self.selected_x) >= 3:
@@ -160,7 +173,8 @@ class Calibration_image(object):
         # Find indices of points, which have to be deleted
         for i in range(len(x_del[0])):
             x_del_local = self.closest_point(x_del[:, i])
-            ind.append(int(where((x_del_local[0, 0] == self.x[0]) * (x_del_local[1, 0] == self.x[1]))[0][0]))
+            ind.append(int(where((x_del_local[0, 0] == self.x[0]) * \
+                                 (x_del_local[1, 0] == self.x[1]))[0][0]))
         # Delete points:
         self.x = delete(self.x, ind, 1)
 
@@ -176,9 +190,12 @@ class Calibration_image(object):
 
         from numpy import array, vstack, hstack
 
-        self.center = self.closest_point(array([self.selected_x[0], self.selected_y[0]]))
-        x_axis = self.closest_point(array([self.selected_x[1], self.selected_y[0]]))
-        y_axis = self.closest_point(array([self.selected_x[2], self.selected_y[2]]))
+        self.center = self.closest_point(array([self.selected_x[0],\
+                                                self.selected_y[0]]))
+        x_axis = self.closest_point(array([self.selected_x[1],\
+                                           self.selected_y[0]]))
+        y_axis = self.closest_point(array([self.selected_x[2],\
+                                           self.selected_y[2]]))
 
         self.axes = vstack((array([[0, 1, 0], [0, 0, 1]]),\
                  hstack((self.center, x_axis, y_axis))))
@@ -188,15 +205,16 @@ class Calibration_image(object):
         are defined, the object coordinates corresponding to the other ellipse
         centerpoints can now be defined. The function takes two optional input
         arguments:
-        - X_spacing, is a list or float, that defines the space between two dots
-        in physical space
-        - Pixeltol, how close two points have to be to each other, before it can
-        be assumed that it is the same point.
+        - X_spacing, is a list or float, that defines the space between two 
+        dots in physical space
+        - Pixeltol, how close two points have to be to each other, before it 
+        can be assumed that it is the same point.
 
         >>> self.object_coordinates(X_spacing = opt., pixeltol = opt.)
         """
 
-        from numpy import vstack, ones, isclose, where, arange, sort, zeros, array
+        from numpy import vstack, ones, isclose, where, arange, sort, zeros,\
+                            array
         from numpy.linalg import lstsq
 
         # If the spacing argument is only given as a single value, assume
@@ -216,8 +234,10 @@ class Calibration_image(object):
         test_on_x = a_x * self.x[0] + b_x
         test_on_y = a_y * self.x[0] + b_y
         # Extract points that are close enough to axis:
-        x_axis = self.x[0:2, isclose(test_on_x, self.x[1], atol = pixeltol, rtol = 0)]
-        y_axis = self.x[0:2, isclose(test_on_y, self.x[1], atol = pixeltol, rtol = 0)]
+        x_axis = self.x[0:2, isclose(test_on_x, self.x[1],\
+                                     atol = pixeltol, rtol = 0)]
+        y_axis = self.x[0:2, isclose(test_on_y, self.x[1],\
+                                     atol = pixeltol, rtol = 0)]
         if self.axes[2, 0] < self.axes[2,1]:
             x_axis = x_axis[:, x_axis[0].argsort()]
         else:
@@ -227,20 +247,23 @@ class Calibration_image(object):
         else:
             y_axis = y_axis[:, (y_axis[1]).argsort()]
         ind_center = [int(where((x_axis[0] == self.center[0]) * \
-                 (x_axis[1] == self.center[1]))[0][0]), int(where((y_axis[0] == self.center[0]) \
+                 (x_axis[1] == self.center[1]))[0][0]),\
+                int(where((y_axis[0] == self.center[0]) \
                  * (y_axis[1] == self.center[1]))[0][0])]
         X_axis = vstack((arange(len(x_axis[0])) - \
                  where((x_axis[0] == self.center[0]) * \
                  (x_axis[1] == self.center[1]))[0][0], zeros(len(x_axis[0]))))
         Y_axis = vstack((zeros(len(y_axis[0])), -arange(len(y_axis[0])) + \
-                 where((y_axis[0] == self.center[0]) * (y_axis[1] == self.center[1]))[0][0]))
+                 where((y_axis[0] == self.center[0]) *\
+                       (y_axis[1] == self.center[1]))[0][0]))
         # Make a grid form to simplify iteration indexing:
         X_grid = zeros((4, len(Y_axis[0]), len(X_axis[0])))
         X_grid[0:2, ind_center[1], :] = X_axis
         X_grid[2:4, ind_center[1], :] = x_axis
         X_grid[0:2, :, ind_center[0]] = Y_axis
         X_grid[2:4, :, ind_center[0]] = y_axis
-        # The points along the x and y axis are now defined, the rest can now be filled out:
+        # The points along the x and y axis are now defined, the rest can now
+        # be filled out:
         """
         The image is divided in four quadrants, that are processed separately.
         Gived the four points:
@@ -256,36 +279,56 @@ class Calibration_image(object):
             i = int(i)
             for j in sort(Y_axis[1, Y_axis[1] > 0]):
                 j = int(j)
-                X_grid[0:2, ind_center[1] - j, ind_center[0] + i] = array([i, j])
-                x_point = X_grid[2:4, ind_center[1] - j + 1, ind_center[0] + i] + \
-                          X_grid[2:4, ind_center[1] - j , ind_center[0] + i - 1] - \
-                          X_grid[2:4, ind_center[1] - j + 1, ind_center[0] + i - 1]
-                X_grid[2:4, ind_center[1] - j, ind_center[0] + i] = self.closest_point(x_point).T[0]
+                X_grid[0:2, ind_center[1] - j, ind_center[0] + i] =\
+                                                                  array([i, j])
+                x_point = X_grid[2:4, ind_center[1] - j + 1,\
+                                      ind_center[0] + i] +\
+                          X_grid[2:4, ind_center[1] - j ,\
+                                      ind_center[0] + i - 1] - \
+                          X_grid[2:4, ind_center[1] - j + 1,\
+                                      ind_center[0] + i - 1]
+                X_grid[2:4, ind_center[1] - j, ind_center[0] + i] =\
+                                              self.closest_point(x_point).T[0]
             for j in Y_axis[1, Y_axis[1] < 0]:
                 j = int(j)
-                X_grid[0:2, ind_center[1] - j, ind_center[0] + i] = array([i, j])
-                x_point = X_grid[2:4, ind_center[1] - j - 1, ind_center[0] + i] + \
-                          X_grid[2:4, ind_center[1] - j , ind_center[0] + i - 1] - \
-                          X_grid[2:4, ind_center[1] - j - 1, ind_center[0] + i - 1]
-                X_grid[2:4, ind_center[1] - j, ind_center[0] + i] = self.closest_point(x_point).T[0]
+                X_grid[0:2, ind_center[1] - j, ind_center[0] + i] =\
+                                                                  array([i, j])
+                x_point = X_grid[2:4, ind_center[1] - j - 1,\
+                                      ind_center[0] + i] + \
+                          X_grid[2:4, ind_center[1] - j ,\
+                                      ind_center[0] + i - 1] - \
+                          X_grid[2:4, ind_center[1] - j - 1,\
+                                      ind_center[0] + i - 1]
+                X_grid[2:4, ind_center[1] - j, ind_center[0] + i] =\
+                                              self.closest_point(x_point).T[0]
 
         for i in X_axis[0, X_axis[0] < 0][::-1]:
             i = int(i)
             for j in sort(Y_axis[1, Y_axis[1] > 0]):
                 j = int(j)
-                X_grid[0:2, ind_center[1] - j, ind_center[0] + i] = array([i, j])
-                x_point = X_grid[2:4, ind_center[1] - j + 1, ind_center[0] + i] + \
-                          X_grid[2:4, ind_center[1] - j , ind_center[0] + i + 1] - \
-                          X_grid[2:4, ind_center[1] - j + 1, ind_center[0] + i + 1]
-                X_grid[2:4, ind_center[1] - j, ind_center[0] + i] = self.closest_point(x_point).T[0]
+                X_grid[0:2, ind_center[1] - j, ind_center[0] + i] =\
+                                                                  array([i, j])
+                x_point = X_grid[2:4, ind_center[1] - j + 1,\
+                                      ind_center[0] + i] + \
+                          X_grid[2:4, ind_center[1] - j ,\
+                                      ind_center[0] + i + 1] - \
+                          X_grid[2:4, ind_center[1] - j + 1,\
+                                      ind_center[0] + i + 1]
+                X_grid[2:4, ind_center[1] - j, ind_center[0] + i] =\
+                                              self.closest_point(x_point).T[0]
 
             for j in Y_axis[1, Y_axis[1] < 0]:
                 j = int(j)
-                X_grid[0:2, ind_center[1] - j, ind_center[0] + i] = array([i, j])
-                x_point = X_grid[2:4, ind_center[1] - j - 1, ind_center[0] + i] + \
-                          X_grid[2:4, ind_center[1] - j , ind_center[0] + i + 1] - \
-                          X_grid[2:4, ind_center[1] - j - 1, ind_center[0] + i + 1]
-                X_grid[2:4, ind_center[1] - j, ind_center[0] + i] = self.closest_point(x_point).T[0]
+                X_grid[0:2, ind_center[1] - j, ind_center[0] + i] =\
+                                                                  array([i, j])
+                x_point = X_grid[2:4, ind_center[1] - j - 1,\
+                                      ind_center[0] + i] + \
+                          X_grid[2:4, ind_center[1] - j ,\
+                                      ind_center[0] + i + 1] - \
+                          X_grid[2:4, ind_center[1] - j - 1,\
+                                      ind_center[0] + i + 1]
+                X_grid[2:4, ind_center[1] - j, ind_center[0] + i] =\
+                                              self.closest_point(x_point).T[0]
         
         # Save the grid in the object:
         self.X_grid = X_grid
